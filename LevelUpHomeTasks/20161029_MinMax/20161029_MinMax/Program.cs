a93308ab129ec0a965d65a2f961837ba378cf12a
SergeySelyutin <casper8706@gmail.com>
2016-10-29 22:00:27 +0300

    20161029

---
+++ LevelUpHomeTasks/20161029_MinMax/20161029_MinMax/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20161029_MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод пяти чисел из консоли
+            Console.Write("Введите первое число: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int second = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int third = int.Parse(Console.ReadLine());
+            Console.Write("Введите четвертое число: ");
+            int fourth = int.Parse(Console.ReadLine());
+            Console.Write("Введите пятое число: ");
+            int fifth = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //поиск max/min тупо влоб
+            MinMax.minMaxBrute(first, second, third, fourth, fifth);
+
+            //Поиск max/min с помощью доп метода без цикла
+            MinMax.minMaxMethodWithoutLoops(first, second, third, fourth, fifth);
+
+            //Поиск max/min с помощью цикла и массива
+            MinMax.minMaxLoop();
+            
+        }
+    }
+
+    class MinMax
+    {
+        private static int max(int a, int b)
+        {
+            return (a > b) ? a : b;     //Помню, что не нужно использовать тернарный оператор, но тут простой пример :)
+        }
+
+        private static int min(int a, int b)
+        {
+            return (a < b) ? a : b;     //Помню, что не нужно использовать тернарный оператор, но тут простой пример :)
+        }
+
+        public static void minMaxBrute(int first, int second, int third, int fourth, int fifth)
+        {
+            //ввод чисел
+            int max = first;
+            int min = first;
+
+            if (max < second)
+            {
+                max = second;
+            }
+            if (max < third)
+            {
+                max = third;
+            }
+            if (max < fourth)
+            {
+                max = fourth;
+            }
+            if (max < fifth)
+            {
+                max = fifth;
+            }
+
+            if (min > second)
+            {
+                min = second;
+            }
+            if (min > third)
+            {
+                min = third;
+            }
+            if (min > fourth)
+            {
+                min = fourth;
+            }
+            if (min > fifth)
+            {
+                min = fifth;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Min/Max влоб:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Max = {0}", max);
+            Console.WriteLine("Min = {0}\n", min);
+        }
+
+        public static void minMaxMethodWithoutLoops(int first, int second, int third, int fourth, int fifth)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Min/Max с помощью метода без циклов:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Max = {0}", MinMax.max(MinMax.max(MinMax.max(first, second), MinMax.max(third, fourth)), fifth));
+            Console.WriteLine("Min = {0}\n", MinMax.min(MinMax.min(MinMax.min(first, second), MinMax.min(third, fourth)), fifth));
+        }
+
+        public static void minMaxLoop()
+        {
+            const int n = 5;
+            int i = 0;
+            int[] arr = new int[n];
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (i = 0; i < n; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Введите {0} число: ", i + 1);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                arr[i] = int.Parse(Console.ReadLine());
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+
+            Console.WriteLine("For Loop:");
+            Console.WriteLine("Max = {0}", max);
+            Console.WriteLine("Min = {0}\n", min);
+
+            //Поиск max/min в цикле do..while
+            max = int.MinValue;
+            min = int.MaxValue;
+            i = 0;
+            do
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                ++i;
+            } while (i < n);
+            Console.WriteLine("do..while loop:");
+            Console.WriteLine("Max = {0}", max);
+            Console.WriteLine("Min = {0}\n", min);
+
+            //Поиск max/min с помощью цикла while
+            max = int.MinValue;
+            min = int.MaxValue;
+            i = 0;
+            while (i < n)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                ++i;
+            }
+            Console.WriteLine("while loop:");
+            Console.WriteLine("Max = {0}", max);
+            Console.WriteLine("Min = {0}\n", min);
+
+            //Поиск max/min с помощью цикла foreach
+            max = int.MinValue;
+            min = int.MaxValue;
+            foreach (int tmp in arr)
+            {
+                if (max < tmp)
+                {
+                    max = tmp;
+                }
+                if (min > tmp)
+                {
+                    min = tmp;
+                }
+                ++i;
+            }
+            Console.WriteLine("foreach loop:");
+            Console.WriteLine("Max = {0}", max);
+            Console.WriteLine("Min = {0}\n", min);
+        }
+    }
+}
---
+++ LevelUpHomeTasks/20161029_UnicodeCharTable/20161029_UnicodeCharTable/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrect = false; //корректность ввода данных
+            ushort limit1 = 0;      //граница диапазона выводимых символов
+            ushort limit2 = 0;      //граница диапазона выводимых символов
+
+            //ввод диапазона выводимых символов
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Введите диапазон вывода символов Юникод через enter: ");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                isCorrect = ushort.TryParse(Console.ReadLine(), out limit1);
+                if (!isCorrect)
+                {
+                    isCorrect = false;
+                    continue;
+                }
+                isCorrect = ushort.TryParse(Console.ReadLine(), out limit2);
+            } while (!isCorrect);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Введите количество столбцов для вывода: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            byte columnCount = byte.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Введите количество строк в каждом блоке для вывода: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            byte rowCount = byte.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            //Если нижняя граница больше верхней - меняем местами
+            if (limit1 > limit2)
+            {
+                ushort tmp = limit2;
+                limit2 = limit1;
+                limit1 = tmp;
+            }
+
+            //вывод символов в консоль
+            ushort i = limit1;      //счетчик для вывода чаров
+            Console.SetCursorPosition(2, 0);
+            int curTopMax = 0;      //самая нижняя позиция курсора в консоли (для корректного вывода строки после таблицы)
+
+            //Вывод таблицы
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            while (i <= limit2)     //Переходим на новую строку групп
+            {
+                for (int currentColumn = 0; (currentColumn < columnCount) && (i <= limit2); currentColumn++)    //переходим по столбцам внутри одной строки групп
+                {
+                    for (int currentRow = 0; (currentRow < rowCount) && (i <= limit2); currentRow++)            //Переходим по строкам внутри группы
+                    {
+                        Console.Write("{0} : {1}", i, (char)i++);
+                        Console.SetCursorPosition(currentColumn * 11 + 2, Console.CursorTop + 1);
+                    }
+                    if (curTopMax < Console.CursorTop)      //запоминаем самую нижнюю позицию курсора
+                    {
+                        curTopMax = Console.CursorTop;
+                    }
+                    if (Console.CursorTop >= rowCount)
+                    {                                                                                               //избавляемся от ошибки System.ArgumentOutOfRangeException.
+                        Console.SetCursorPosition((currentColumn + 1) * 11 + 2, Console.CursorTop - rowCount);      //(Console.CursorTop - rowCount) может принимать отрицательные значения
+                    }
+                }
+                Console.SetCursorPosition(2, Console.CursorTop + rowCount + 1);
+            }
+            Console.SetCursorPosition(0, curTopMax + 2);
+        }
+    }
+}
